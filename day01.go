b449299415bfb71d749eddfb79537289d8b96163
Jakub Głazik <rzabcio@gmail.com>
2022-12-01 20:55:20 +0100

    Day 1 part 1 - solved.

--- day01.go
+++ day01.go
@@ -1,20 +1,58 @@
 package main
 
 import (
-// "fmt"
-// "regexp"
-// "strconv"
-// "strings"
+	"fmt"
+	// "regexp"
+	"strconv"
+	// "strings"
+
+	"github.com/thoas/go-funk"
 )
 
-func Day1_1(filename string) int {
-	result := 0
-
-	return result
+func Day1_1(filename string) (result int) {
+	f := NewFellowship(filename)
+	_, result = minMax(f.totals)
+	fmt.Printf("==> backpack with most rations has %d calories\n", result)
+	return
 }
 
-func Day1_2(filename string) int {
-	result := 0
+func Day1_2(filename string) (result int) {
+	// f := NewFellowship(filename)
 
-	return result
+	return
+}
+
+type Fellowship struct {
+	backpacks []Backpack
+	totals    []int
+}
+
+func NewFellowship(filename string) Fellowship {
+	f := new(Fellowship)
+	f.backpacks = make([]Backpack, 0)
+	f.totals = make([]int, 0)
+	backpack := *new(Backpack)
+
+	for line := range inputCh(filename) {
+		if len(line) == 0 {
+			f.backpacks = append(f.backpacks, backpack)
+			f.totals = append(f.totals, backpack.sum())
+			backpack = *new(Backpack)
+		} else {
+			ration, _ := strconv.Atoi(line)
+			backpack.rations = append(backpack.rations, ration)
+		}
+	}
+	f.backpacks = append(f.backpacks, backpack)
+	f.totals = append(f.totals, backpack.sum())
+	return *f
+}
+
+type Backpack struct {
+	rations []int
+}
+
+func (b *Backpack) sum() (r int) {
+	r = funk.Reduce(b.rations, '+', 0).(int)
+	return
 }
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestDay1(t *testing.T) {
-	got, want := Day1_1("input-files/day01-test1.txt"), 1512
+	got, want := Day1_1("input-files/day01-test1.txt"), 24000
 	if got != want {
 		t.Errorf("Day1_1(test1) = %d; want %d", got, want)
 	}
