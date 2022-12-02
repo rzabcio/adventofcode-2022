3a5155d69228f2cf38e176e85e3a2af671122340
Jakub Głazik <rzabcio@gmail.com>
2022-12-02 15:27:23 +0100

    Day 3 part 1 - solved.

---
+++ day03.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	// "regexp"
+	// "strconv"
+	"strings"
+
+	"github.com/thoas/go-funk"
+	// "github.com/thoas/go-funk"
+)
+
+func Day03_1(filename string) (result int) {
+	rs := NewRucksacks(filename)
+	for _, s := range rs.rucksacks {
+		result += s.invalidVal
+	}
+	fmt.Printf("03.1 ==> sum of invalid items is %d\n", result)
+	return
+}
+
+func Day03_2(filename string) (result int) {
+	fmt.Printf("03.1 ==> %d\n", result)
+	return
+}
+
+type Rucksacks struct {
+	rucksacks []Sack
+}
+
+func NewRucksacks(filename string) (rs *Rucksacks) {
+	rs = new(Rucksacks)
+	rs.rucksacks = make([]Sack, 0)
+
+	for line := range inputCh(filename) {
+		rs.rucksacks = append(rs.rucksacks, *NewSack(line))
+	}
+
+	return rs
+}
+
+type Sack struct {
+	l           []string
+	r           []string
+	invalid     []string
+	invalidVal  int
+	invalidVal2 int
+}
+
+func NewSack(line string) (s *Sack) {
+	s = new(Sack)
+	split := strings.Split(line, "")
+	halfLen := len(split) / 2
+	s.l = split[:halfLen:halfLen]
+	s.r = split[halfLen:len(split):len(split)]
+	s.invalid = funk.UniqString(funk.IntersectString(s.l, s.r))
+	s.invalidVal = int([]byte(s.invalid[0])[0])
+	if s.invalidVal > 96 {
+		s.invalidVal -= 96
+	} else {
+		s.invalidVal -= 38
+	}
+
+	return s
+}
